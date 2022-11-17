ad59b6c005df84b87fcb5ab9ef186dd9a1629f7b
Ricky Chu <rchu6120@gmail.com>
2022-11-17 16:06:05 -0500

    updated airline staff and customer login

---
+++ flask_project/init1.py
@@ -0,0 +1,227 @@
+# Import Flask Library
+from flask import Flask, render_template, request, session, url_for, redirect
+import pymysql.cursors
+
+# Initialize the app from Flask
+app = Flask(__name__)
+
+# Configure MySQL
+conn = pymysql.connect(host='localhost',
+                       port=8889,
+                       user='root',
+                       password='root',
+                       db='air_system',
+                       charset='utf8mb4',
+                       cursorclass=pymysql.cursors.DictCursor)
+
+
+# Define route for index
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+# Define route for login
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+# Define route for customer login
+@app.route('/customer_login')
+def customer_login():
+    return render_template('customer_login.html')
+
+
+# Define route for airline staff login
+@app.route('/airline_staff_login')
+def airline_staff_login():
+    return render_template('airline_staff_login.html')
+
+
+# Authenticates the customer login
+@app.route('/customer_login_auth', methods=['GET', 'POST'])
+def customer_login_auth():
+    # grabs information from the forms
+    email = request.form['email']
+    password = request.form['password']
+
+    # cursor used to send queries
+    cursor = conn.cursor()
+    # executes query
+    query = 'SELECT * FROM customer WHERE EmailAddress = %s and Password = %s'
+    cursor.execute(query, (email, password))
+    # stores the results in a variable
+    data = cursor.fetchone()
+    # use fetchall() if you are expecting more than 1 data row
+    cursor.close()
+    error = None
+    if data:
+        # creates a session for the user
+        # session is a built in
+        session['email'] = email
+        return redirect(url_for('customer_home'))
+    else:
+        # returns an error message to the html page
+        error = 'Invalid login or username'
+        return render_template('login.html', error=error)
+
+
+# Authenticates the airline staff login
+@app.route('/airline_staff_login_auth', methods=['GET', 'POST'])
+def airline_staff_login_auth():
+    # grabs information from the forms
+    username = request.form['username']
+    password = request.form['password']
+
+    # cursor used to send queries
+    cursor = conn.cursor()
+    # executes query
+    query = 'SELECT * FROM airline_staff WHERE Username = %s and Password = %s'
+    cursor.execute(query, (username, password))
+    # stores the results in a variable
+    data = cursor.fetchone()
+    # use fetchall() if you are expecting more than 1 data row
+    cursor.close()
+    error = None
+    if data:
+        # creates a session for the user
+        # session is a built in
+        session['username'] = username
+        return redirect(url_for('airline_staff_home'))
+    else:
+        # returns an error message to the html page
+        error = 'Invalid login or username'
+        return render_template('login.html', error=error)
+
+
+# Define route for register
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
+# Define route for customer register
+@app.route('/customer_register')
+def customer_register():
+    return render_template('customer_register.html')
+
+
+# Define route for airline staff register
+@app.route('/airline_staff_register')
+def airline_staff_register():
+    return render_template('airline_staff_register.html')
+
+
+# Authenticates the customer register
+@app.route('/customer_register_auth', methods=['GET', 'POST'])
+def customer_register_auth():
+    # grabs information from the forms
+    email = request.form['email']
+    password = request.form['password']
+
+    # cursor used to send queries
+    cursor = conn.cursor()
+    # executes query
+    query = 'SELECT * FROM purchase WHERE EmailAddress = %s ORDER BY PurchaseDateandTime DESC'
+    cursor.execute(query, email)
+    # stores the results in a variable
+    data = cursor.fetchone()
+    # use fetchall() if you are expecting more than 1 data row
+    error = None
+    if data:
+        # If the previous query returns data, then user exists
+        error = "This user already exists"
+        return render_template('register.html', error=error)
+    else:
+        ins = 'INSERT INTO user VALUES(%s, %s)'
+        cursor.execute(ins, (email, password))
+        conn.commit()
+        cursor.close()
+        return render_template('index.html')
+
+
+# Authenticates the airline staff register
+@app.route('/airline_staff_register_auth', methods=['GET', 'POST'])
+def airline_staff_register_auth():
+    # grabs information from the forms
+    username = request.form['username']
+    password = request.form['password']
+
+    # cursor used to send queries
+    cursor = conn.cursor()
+    # executes query
+    query = 'SELECT * FROM ticket WHERE AirlineName = %s'
+    cursor.execute(query, username)
+    # stores the results in a variable
+    data = cursor.fetchone()
+    # use fetchall() if you are expecting more than 1 data row
+    error = None
+    if data:
+        # If the previous query returns data, then user exists
+        error = "This user already exists"
+        return render_template('register.html', error=error)
+    else:
+        ins = 'INSERT INTO user VALUES(%s, %s)'
+        cursor.execute(ins, (username, password))
+        conn.commit()
+        cursor.close()
+        return render_template('index.html')
+
+
+@app.route('/customer_home')
+def customer_home():
+    email = session['email']
+    cursor = conn.cursor();
+    query = 'SELECT * FROM purchase WHERE EmailAddress = %s ORDER BY PurchaseDateandTime DESC'
+    cursor.execute(query, email)
+    data1 = cursor.fetchall()
+    for each in data1:
+        print(each['TicketIDNumber'])
+    cursor.close()
+    return render_template('customer_home.html', email=email, posts=data1)
+
+
+@app.route('/airline_staff_home')
+def airline_staff_home():
+    username = session['username']
+    cursor = conn.cursor();
+    query = 'SELECT * FROM ticket WHERE AirlineName = %s'
+    cursor.execute(query, username)
+    data1 = cursor.fetchall()
+    for each in data1:
+        print(each['TicketIDNumber'])
+    cursor.close()
+    return render_template('airline_staff_home.html', username=username, posts=data1)
+
+
+@app.route('/post', methods=['GET', 'POST'])
+def post():
+    username = session['username']
+    cursor = conn.cursor();
+    blog = request.form['blog']
+    query = 'INSERT INTO blog (blog_post, username) VALUES(%s, %s)'
+    cursor.execute(query, (blog, username))
+    conn.commit()
+    cursor.close()
+    return redirect(url_for('home'))
+
+
+@app.route('/customer_logout')
+def customer_logout():
+    session.pop('email')
+    return redirect('/')
+
+
+@app.route('/airline_staff_logout')
+def airline_staff_logout():
+    session.pop('username')
+    return redirect('/')
+
+
+app.secret_key = 'some key that you will never guess'
+# Run the app on localhost port 5000
+# debug = True -> you don't have to restart flask
+# for changes to go through, TURN OFF FOR PRODUCTION
+if __name__ == "__main__":
+    app.run('127.0.0.1', 5000, debug=True)
